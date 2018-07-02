d3a7d356418e93ee2ec2b910f1e126f420aa81e9
Sylvie <numuhire_s@yahoo.fr>
2018-07-02 16:45:40 +0200

    création guerrier, mage et paysan

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+var guerrier = {
+    nom: "Aragorn",
+    age: 30,
+    taille: 180,
+    arme:"arc",
+    vie: 100,
+};
+
+var mage = {
+    nom: "Saruman",
+    age: 70,
+    taille: 190,
+    arme: "baguette",
+    vie: 200,
+};
+
+var paysan = {
+    nom: "Frodon",
+    age: 25,
+    taille: 110,
+    arme: "anneau",
+    vie: 150,
+};
