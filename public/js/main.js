48525edf6d8eec732be11e14caa9c7d7a530d129
Sylvie <numuhire_s@yahoo.fr>
2018-07-03 09:37:59 +0200

    modification vies et attaques

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,8 @@ var guerrier = {
     age: 30,
     taille: 180,
     arme:"arc",
-    vie: 100,
-    attaque: 500,
+    vie: 500,
+    attaque: 100,
 };
 
 var mage = {
@@ -12,8 +12,8 @@ var mage = {
     age: 70,
     taille: 190,
     arme: "baguette",
-    vie: 200,
-    attaque: 700,
+    vie: 1000,
+    attaque: 200,
 };
 
 var paysan = {
@@ -21,25 +21,25 @@ var paysan = {
     age: 25,
     taille: 110,
     arme: "anneau",
-    vie: 150,
-    attaque: 1000,
+    vie: 700,
+    attaque: 150,
 };
 
 var longueur_nom_guerrier = guerrier.nom.length;
 var longueur_arme_guerrier = guerrier.arme.length;
 var deuxieme_lettre_nom_guerrier = guerrier.nom.charAt(1);
-var lettres_1_a_3_nom_guerrier = guerrier.nom.slice(0,3)
+var lettres_1_a_3_nom_guerrier = guerrier.nom.slice(0,3);
 var nom_et_age_guerrier = guerrier.nom.concat(" " + guerrier.age);
 
 var longueur_nom_mage = mage.nom.length;
 var longueur_arme_mage = mage.arme.length;
 var deuxieme_lettre_nom_mage = mage.nom.charAt(1);
-var lettres_1_a_3_nom_mage = mage.nom.slice(0,3)
+var lettres_1_a_3_nom_mage = mage.nom.slice(0,3);
 var nom_et_age_mage = mage.nom.concat(" " + mage.age);
 
 
 var longueur_nom_paysan = paysan.nom.length;
 var longueur_arme_paysan = paysan.arme.length;
 var deuxieme_lettre_nom_paysan = paysan.nom.charAt(1);
-var lettres_1_a_3_nom_payasan = paysan.nom.slice(0,3)
+var lettres_1_a_3_nom_payasan = paysan.nom.slice(0,3);
 var nom_et_age_paysan = paysan.nom.concat(" " + paysan.age);
